2670f4366e23a1a56968f17e86453eed27e07cd1
Laisky <ppcelery@gmail.com>
2018-10-09 09:52:11 +0800

    feat(paas-151): send msg patch to downstream

--- producer.go
+++ producer.go
@@ -30,85 +30,126 @@ func NewProducer(addr string, msgChan <-chan *FluentMsg, msgPool *sync.Pool) *Pr
 
 // Run starting <n> Producer to send messages
 func (p *Producer) Run(fork int, commitChan chan<- int64) {
-	wg := &sync.WaitGroup{}
-	for i := 0; i < fork; i++ {
-		wg.Add(1)
-		go func(n int) {
-			defer wg.Done()
-			utils.Logger.Info("start producer", zap.Int("n", n), zap.String("addr", p.addr))
+	utils.Logger.Info("start producer", zap.String("addr", p.addr))
+
+	var (
+		msg                *FluentMsg
+		retryMsgChan       = make(chan *FluentMsg, 500)
+		producerTagChanMap = map[string]chan<- *FluentMsg{}
+		ok                 bool
+	)
+
+	for {
+		select {
+		case msg = <-retryMsgChan:
+		case msg = <-p.msgChan:
+		}
+
+		if _, ok = producerTagChanMap[msg.Tag]; !ok {
+			producerTagChanMap[msg.Tag] = p.SpawnForTag(fork, msg.Tag, commitChan)
+		}
+		producerTagChanMap[msg.Tag] <- msg
+	}
+
+}
+
+func (p *Producer) SpawnForTag(fork int, tag string, commitChan chan<- int64) chan<- *FluentMsg {
+	utils.Logger.Info("SpawnForTag", zap.Int("fork", fork), zap.String("tag", tag))
+	var (
+		inChan = make(chan *FluentMsg, 1000)
+	)
+
+	for i := 0; i < fork; i++ { // parallel to each tag
+		go func() {
+			defer utils.Logger.Error("producer exits", zap.String("tag", tag))
 
 			var (
-				nRetry       = 0
-				maxRetry     = 3
-				id           int64
-				retryMsgChan = make(chan *FluentMsg, 50)
-				msg          *FluentMsg
-				encoder      *Encoder
+				nRetry           = 0
+				maxRetry         = 3
+				id               int64
+				retryMsgChan     = make(chan *FluentMsg, 50)
+				msg              *FluentMsg
+				maxNBatch        = 100
+				msgBatch         = make([]*FluentMsg, maxNBatch)
+				msgBatchDelivery []*FluentMsg
+				iBatch           = 0
+				lastT            = time.Unix(0, 0)
+				maxWait          = 10 * time.Second
+				encoder          *Encoder
 			)
 
 		RECONNECT:
 			conn, err := net.DialTimeout("tcp", p.addr, 10*time.Second)
 			if err != nil {
-				utils.Logger.Error("try to connect to backend got error", zap.Error(err), zap.Int("n", n))
-				time.Sleep(5 * time.Second)
+				utils.Logger.Error("try to connect to backend got error", zap.Error(err), zap.String("tag", tag))
+				time.Sleep(1 * time.Second)
 				goto RECONNECT
 			}
 			defer conn.Close()
-			utils.Logger.Info("connected to backend", zap.String("backend", conn.RemoteAddr().String()))
+			utils.Logger.Info("connected to backend",
+				zap.String("backend", conn.RemoteAddr().String()),
+				zap.String("tag", tag))
 
 			encoder = NewEncoder(conn)
-			for {
-				select {
-				case msg = <-retryMsgChan:
-				case msg = <-p.msgChan:
+			for msg = range inChan {
+				msgBatch[iBatch] = msg
+				iBatch++
+				if iBatch < maxNBatch &&
+					time.Now().Sub(lastT) < maxWait {
+					continue
 				}
+				lastT = time.Now()
+				msgBatchDelivery = msgBatch[:iBatch]
+				iBatch = 0
 
 				nRetry = 0
 				for {
 					if utils.Settings.GetBool("dry") {
 						utils.Logger.Info("send message to backend",
-							zap.String("tag", msg.Tag),
-							zap.String("log", fmt.Sprintf("%v", msg.Message)),
-							zap.Int("n", n))
+							zap.String("tag", tag),
+							zap.String("log", fmt.Sprintf("%v", msgBatch[0].Message)))
 						goto FINISHED
 					}
 
-					err = encoder.Encode(msg)
-					if err != nil {
+					if err = encoder.EncodeBatch(tag, msgBatchDelivery); err != nil {
 						nRetry++
 						if nRetry > maxRetry {
-							utils.Logger.Error("try send message got error",
-								zap.Error(err),
-								zap.Int("n", n),
-								zap.String("tag", msg.Tag))
+							utils.Logger.Error("try send message got error", zap.Error(err), zap.String("tag", tag))
 
-							retryMsgChan <- msg
-							conn.Close()
-							utils.Logger.Warn("connection closed, try to reconnect...")
+							for _, msg = range msgBatchDelivery {
+								retryMsgChan <- msg
+							}
+
+							if err = conn.Close(); err != nil {
+								utils.Logger.Error("try to close connection got error", zap.Error(err))
+							}
+							utils.Logger.Info("connection closed, try to reconnect...")
 							goto RECONNECT
 						}
 
-						time.Sleep(1 * time.Second)
+						time.Sleep(100 * time.Microsecond)
 						continue
 					}
 
-					utils.Logger.Debug("success sent message to backend", zap.String("backend", p.addr))
+					utils.Logger.Debug("success sent message to backend", zap.String("backend", p.addr), zap.String("tag", tag))
 					goto FINISHED
 				}
 
 			FINISHED:
-				commitChan <- msg.Id
-				if msg.extIds != nil {
-					for _, id = range msg.extIds {
-						commitChan <- id
+				for _, msg = range msgBatchDelivery {
+					commitChan <- msg.Id
+					if msg.extIds != nil {
+						for _, id = range msg.extIds {
+							commitChan <- id
+						}
 					}
-				}
 
-				msg.extIds = nil
-				p.msgPool.Put(msg)
+					msg.extIds = nil
+					p.msgPool.Put(msg)
+				}
 			}
-		}(i)
+		}()
 	}
 
-	wg.Wait()
+	return inChan
 }

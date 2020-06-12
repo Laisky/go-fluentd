fe052f1ccfdeaa893a0ce7b0c2368be542ba8776
Laisky <ppcelery@gmail.com>
2020-06-12 18:24:06 +0800

    feat: `add` support extract value from map & variable

--- libs/add_test.go
+++ libs/add_test.go
@@ -10,12 +10,38 @@ func Test_replaceByKey(t *testing.T) {
 		msg *FluentMsg
 		v   string
 	}
-	msg := &FluentMsg{
+	msgOrig := &FluentMsg{
 		Message: map[string]interface{}{
 			"a":     "aaaa",
 			"b":     "bbbb",
+			"LA":    "AAAA",
+			"LB":    "BBBB",
 			"int":   123,
 			"float": 1.21,
+			"in": map[string]interface{}{
+				"ia":  "iaaaa",
+				"ib":  "ibbbb",
+				"iLA": "iAAAA",
+				"iLB": "iBBBB",
+			},
+		},
+		Id:  123,
+		Tag: "test",
+	}
+	msgArg := &FluentMsg{
+		Message: map[string]interface{}{
+			"a":     "aaaa",
+			"b":     "bbbb",
+			"LA":    "AAAA",
+			"LB":    "BBBB",
+			"int":   123,
+			"float": 1.21,
+			"in": map[string]interface{}{
+				"ia":  "iaaaa",
+				"ib":  "ibbbb",
+				"iLA": "iAAAA",
+				"iLB": "iBBBB",
+			},
 		},
 		Id:  123,
 		Tag: "test",
@@ -26,32 +52,42 @@ func Test_replaceByKey(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
-		{"t0", args{msg, "a"}, "a"},
-		{"t1", args{msg, "%{a}"}, "aaaa"},
-		{"t2", args{msg, "a%{a}"}, "aaaaa"},
-		{"t3", args{msg, "a%{a}a"}, "aaaaaa"},
-		{"t4", args{msg, "v%{a}v"}, "vaaaav"},
-		{"t5", args{msg, "v%%{a}v"}, "v%aaaav"},
-		{"t6", args{msg, "v%{{a}v"}, "v%{{a}v"},
-		{"t7", args{msg, "v%{a}}v"}, "vaaaa}v"},
-		{"t8", args{msg, "%{a}%{a}"}, "aaaaaaaa"},
-		{"t9", args{msg, "%{a}%{b}%{a}"}, "aaaabbbbaaaa"},
-		{"t10", args{msg, "%{int}"}, "123"},
-		{"t11", args{msg, "%{float}"}, "1.21"},
-		{"t11", args{msg, "%{@tag}"}, "test"},
-		{"t11", args{msg, "%{@id}"}, "123"},
+		{"0", args{msgArg, "a"}, "a"},
+		{"1", args{msgArg, "%{a}"}, "aaaa"},
+		{"2", args{msgArg, "%{in}"}, "map[iLA:iAAAA iLB:iBBBB ia:iaaaa ib:ibbbb]"},
+		{"3", args{msgArg, "%{ii}"}, ""},
+		{"4", args{msgArg, "%{in.ia}"}, "iaaaa"},
+		{"5", args{msgArg, "%{in.iLB}"}, "iBBBB"},
+		{"6", args{msgArg, "%{in.ilB}"}, ""},
+		{"7", args{msgArg, "%{@upper:a}"}, "AAAA"},
+		{"8", args{msgArg, "%{@lower:LA}"}, "aaaa"},
+		{"9", args{msgArg, "%{@upper:in.ia}"}, "IAAAA"},
+		{"10", args{msgArg, "%{@lower:in.iLA}"}, "iaaaa"},
+		{"11", args{msgArg, "a%{a}"}, "aaaaa"},
+		{"12", args{msgArg, "a%{a}a"}, "aaaaaa"},
+		{"13", args{msgArg, "v%{a}v"}, "vaaaav"},
+		{"14", args{msgArg, "v%%{a}v"}, "v%aaaav"},
+		{"15", args{msgArg, "v%{{a}v"}, "v%{{a}v"},
+		{"16", args{msgArg, "v%{a}}v"}, "vaaaa}v"},
+		{"17", args{msgArg, "%{a}%{a}"}, "aaaaaaaa"},
+		{"18", args{msgArg, "%{a}%{b}%{a}"}, "aaaabbbbaaaa"},
+		{"19", args{msgArg, "%{int}"}, "123"},
+		{"20", args{msgArg, "%{float}"}, "1.21"},
+		{"21", args{msgArg, "%{@tag}"}, "test"},
+		{"22", args{msgArg, "%{@id}"}, "123"},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ReplaceStrByMsg(tt.args.msg, tt.args.v); got != tt.want {
-				t.Errorf("ReplaceStrByMsg() = %v, want %v", got, tt.want)
-			}
-		})
+		if got := ReplaceStrByMsg(tt.args.msg, tt.args.v); got != tt.want {
+			t.Fatalf("[%s] ReplaceStrByMsg() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+
+	if !reflect.DeepEqual(msgOrig, msgArg) {
+		t.Fatal("should not change msg in the arg")
 	}
 }
 
-func TestLoadValFromMap(t *testing.T) {
+func TestGetValFromMap(t *testing.T) {
 	type args struct {
 		m   interface{}
 		key string
@@ -61,21 +97,32 @@ func TestLoadValFromMap(t *testing.T) {
 		args args
 		want interface{}
 	}{
-		// TODO: Add test cases.
 		{"0", args{map[string]string{"a": "b"}, "a"}, "b"},
-		{"1", args{map[string]string{"a": "b", "b": "a"}, "a"}, "b"},
-		{"2", args{map[string]string{"a": "b", "b": "a"}, "b"}, "a"},
-		{"3", args{map[string]interface{}{"a": "b"}, "a"}, "b"},
-		{"4", args{map[string]interface{}{"a": "b", "b": "a"}, "a"}, "b"},
-		{"5", args{map[string]interface{}{"a": "b", "b": "a"}, "b"}, "a"},
-		{"5", args{map[string]interface{}{"a": "b", "b": map[string]string{"a": "b"}}, "b"}, map[string]string{"a": "b"}},
-		{"5", args{map[string]interface{}{"a": "b", "b": map[string]string{"a": "b"}}, "b.a"}, "b"},
+		{"1", args{map[string]string{"a": "b"}, "a."}, nil},
+		{"2", args{map[string]string{"a": "b"}, "a.b"}, nil},
+		{"3", args{map[string]string{"a": "b"}, "a."}, nil},
+		{"4", args{map[string]string{"a": "b"}, "b"}, nil},
+		{"5", args{map[string]string{"a": "b"}, ""}, nil},
+		{"6", args{map[string]string{"a": "b", "b": "a"}, "a"}, "b"},
+		{"7", args{map[string]string{"a": "b", "b": "a"}, "b"}, "a"},
+		{"8", args{map[string]string{"a": "b", "b": "a"}, "b.a"}, nil},
+		{"9", args{map[string]string{"a": "b", "b": "a"}, "b."}, nil},
+		{"10", args{map[string]string{"a": "b", "b": "a"}, "c"}, nil},
+		{"11", args{map[string]string{"a": "b", "b": "a"}, ""}, nil},
+		{"12", args{map[string]interface{}{"a": "b"}, "a"}, "b"},
+		{"13", args{map[string]interface{}{"a": "b"}, "b"}, nil},
+		{"14", args{map[string]interface{}{"a": "b"}, ""}, nil},
+		{"15", args{map[string]interface{}{"a": "b"}, "a.b"}, nil},
+		{"16", args{map[string]interface{}{"a": "b", "b": "a"}, "a"}, "b"},
+		{"17", args{map[string]interface{}{"a": "b", "b": "a"}, "b"}, "a"},
+		{"18", args{map[string]interface{}{"a": "b", "b": map[string]string{"a": "b"}}, "b"}, map[string]string{"a": "b"}},
+		{"19", args{map[string]interface{}{"a": "b", "b": map[string]string{"a": "b"}}, "b.a"}, "b"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetValFromMap(tt.args.m, tt.args.key); !reflect.DeepEqual(got, tt.want) {
 				// if got := GetValFromMap(tt.args.m, tt.args.key); !interfaceStringEqual(got, tt.want) {
-				t.Errorf("GetValFromMap() = %v, want %v", got, tt.want)
+				t.Fatalf("GetValFromMap() = %v, want %v", got, tt.want)
 			}
 		})
 	}
